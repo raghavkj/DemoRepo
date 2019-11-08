123792ef7bc8e5f0e42c4c3c787b5300d89ae7c2
raghavKalale <rkalale@deloitte.com>
2019-11-08 13:29:07 +0530

    Checking TestBase+1

--- src/main/java/com/qa/libs/TestBase.java
+++ src/main/java/com/qa/libs/TestBase.java
@@ -6,4 +6,12 @@ public class TestBase {
 		System.out.println("****--Test Method--****");
 	}
 
+	public void testBase2() {
+		System.out.println("****--Check Method--****");
+	}
+
+	public void testBase3() {
+		System.out.println("Adding new Method");
+	}
+
 }
